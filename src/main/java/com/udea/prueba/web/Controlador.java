8109a15fa149cad609d448e846fe990e72087ffb
JohnMejia321 <john.mejias@udea.edu.co>
2022-10-15 11:53:59 -0500

    Se implementa metodo post para agregar personas

--- src/main/java/com/udea/prueba/web/Controlador.java
+++ src/main/java/com/udea/prueba/web/Controlador.java
@@ -1,28 +1,43 @@
 package com.udea.prueba.web;
 
 import com.udea.prueba.dao.PersonaDao;
+import com.udea.prueba.domain.Persona;
 import com.udea.prueba.servicio.PersonaService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 @Slf4j
 public class Controlador {
 
-@Autowired
-private PersonaService personaService;
+    @Autowired
+    private PersonaService personaService;
 
-        @GetMapping("/")
-        public String inicio(Model model){
-            var personas=personaService.listarPersonas();
-            log.info("ejecutando el controlador Spring MVC");
-            model.addAttribute("personas", personas);
-            return "index";
-        }
+    @GetMapping("/")
+    public String inicio(Model model) {
+        var personas = personaService.listarPersonas();
+        log.info("ejecutando el controlador Spring MVC");
+        model.addAttribute("personas", personas);
+        return "index";
     }
+
+    @GetMapping("/agregar")
+    public String agregar(Persona persona) {
+        return "modificar";
+    }
+
+
+    @PostMapping("/guardar")
+    public String guardar(Persona persona) {
+        personaService.guardar(persona);
+        return "redirect:/";
+    }
+
+}
 
 
