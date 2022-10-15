f39cbdc76f535762d5552d8eb90aac3c00f8ac89
JohnMejia321 <john.mejias@udea.edu.co>
2022-10-15 13:05:42 -0500

    Se implementa metodo  para eliminar

--- src/main/java/com/udea/prueba/web/Controlador.java
+++ src/main/java/com/udea/prueba/web/Controlador.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -43,8 +44,15 @@ public class Controlador {
        persona= personaService.encontrarPersona(persona);
        model.addAttribute("persona",persona);
        return "modificar";
+    }
+
+    @GetMapping("/eliminar/{idPersona}")
+    public String eliminar(Persona persona){
+        personaService.eliminar(persona);
+        return "redirect:/";
 
     }
+
 
 }
 
